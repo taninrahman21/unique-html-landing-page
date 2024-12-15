f2c12e4438484075bb2d3469c12fe0a2e23b9ae2
taninrahman21 <taninrahman21@gmail.com>
2024-12-16 00:32:24 +0600

    complete four section

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const allAccordionHeader = document.querySelectorAll('.accordion-header');
+const allAccordionBody = document.querySelectorAll('.accordion-item');
+
+allAccordionHeader.forEach(header => {
+  header.addEventListener('click', () => {
+    const item = header.parentElement;
+    const isActive = item.classList.contains('active');
+
+    // Close all accordion items
+    allAccordionBody.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    // Open clicked item if it wasn't active
+    if (!isActive) {
+      item.classList.add('active');
+    }
+  })
+});
